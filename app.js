2306e8fc118bbaa772667418c132f7b16a0b14e5
Fabio M. Godoy <fabiomdg1@gmail.com>
2024-03-17 22:18:39 -0300

    Firts Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const db = require("./db/connection");
 
 const PORT = 3000;
 
@@ -7,6 +8,17 @@ app.listen(PORT, function(){
     console.log("O espress está rodando na porta " + PORT);
 });
 
+
+// Conexão com o banco
+db
+    .authenticate()
+    .then(() => {
+        console.log("Autenticado no banco de dados com sucesso.")
+    }).catch((err) => {
+        console.log("Ocorreu um erro ao autenticar", err);    
+    });
+
+// Rotas
 app.get("/", (req, res)=>{
     res.send("Esta funcionando");
 });
